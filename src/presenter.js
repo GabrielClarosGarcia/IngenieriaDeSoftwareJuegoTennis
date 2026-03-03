a1b50614b9ce16ae0a465daf181b18bab5883d6e
Gabriel Claros <gabriel.claros.g@ucb.edu.bo>
2026-03-03 03:37:58 -0400

    fix: arreglo de error en presenter, que no cargaba bien la UI para probar el juego

--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,21 @@
 import Tennis from "./tennis.js";
 
-const tennis = new Tennis(); // ✅ SOLO UNA VEZ
+document.addEventListener("DOMContentLoaded", () => {
 
-const resultadoDiv = document.querySelector("#resultado-div");
-const player1Button = document.querySelector("#player1-button");
-const player2Button = document.querySelector("#player2-button");
+  const tennis = new Tennis();
 
-player1Button.addEventListener("click", () => {
-  tennis.player1Scores();
-  resultadoDiv.textContent = tennis.score();
+  const resultadoDiv = document.querySelector("#resultado-div");
+  const player1Button = document.querySelector("#player1-button");
+  const player2Button = document.querySelector("#player2-button");
+
+  player1Button.addEventListener("click", () => {
+    tennis.player1Scores();
+    resultadoDiv.textContent = tennis.score();
+  });
+
+  player2Button.addEventListener("click", () => {
+    tennis.player2Scores();
+    resultadoDiv.textContent = tennis.score();
+  });
+
 });
-
-player2Button.addEventListener("click", () => {
-  tennis.player2Scores();
-  resultadoDiv.textContent = tennis.score();
-});ç
