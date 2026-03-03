5fcc6a0f5a3fd45f0054f4e2f443b34d349d7cd2
Gabriel Claros <gabriel.claros.g@ucb.edu.bo>
2026-03-03 03:17:07 -0400

    feat: creacion del UI para el juego, ademas de un arreglo en tennis.js para cuando van empatados 3-3, pasan a 4-3 y vuelven a estar empatados

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,17 @@
-import sumar from "./sumador";
+import Tennis from "./tennis.js";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
+const tennis = new Tennis(); // ✅ SOLO UNA VEZ
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
+const resultadoDiv = document.querySelector("#resultado-div");
+const player1Button = document.querySelector("#player1-button");
+const player2Button = document.querySelector("#player2-button");
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+player1Button.addEventListener("click", () => {
+  tennis.player1Scores();
+  resultadoDiv.textContent = tennis.score();
 });
+
+player2Button.addEventListener("click", () => {
+  tennis.player2Scores();
+  resultadoDiv.textContent = tennis.score();
+});ç
